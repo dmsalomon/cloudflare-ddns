d978d7a4bc34716cf6efcf4a99304a6be48734b1
Dov Salomon <me@dov.ms>
2020-11-14 22:27:16 -0500

    Add argparser for more config options
    The argparser allows the user to supply a custom path for the
    configuration file, use a custom delay between repetitious updates, and
    disable updating of ipv6 (AAAA) records.

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -1,27 +1,29 @@
-import requests, json, sys, os
-import time
+import argparse, requests, json, sys, os, time
 
-PATH = os.getcwd() + "/"
 version = float(str(sys.version_info[0]) + "." + str(sys.version_info[1]))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-4", action="store_true", help="only update IPv4 records")
+parser.add_argument("--config", default="config.json", help="path to configuration file")
+parser.add_argument("--repeat", nargs='?', type=int, default=0, const=600, help="repeat every REPEAT seconds, default is 600 (10 minutes)")
+
+args = parser.parse_args()
+onlyIPv4 = getattr(args, '4')
 
 if(version < 3.5):
     raise Exception("This script requires Python 3.5+")
 
-with open(PATH + "config.json") as config_file:
+with open(args.config) as config_file:
     config = json.loads(config_file.read())
 
 def getIPs():
+    ips = []
+
     a = ""
-    aaaa = ""
     try:
         a = requests.get("https://dns.timknowsbest.com/api/ipv4").text
     except Exception:
         print("Warning: IPv4 not detected.")
-    try:
-        aaaa = requests.get("https://api6.ipify.org?format=json").json().get("ip")
-    except Exception:
-        print("Warning: IPv6 not detected.")
-    ips = []
 
     if(a.find(".") > -1):
         ips.append({
@@ -31,13 +33,20 @@ def getIPs():
     else:
         print("Warning: IPv4 not detected.")
 
-    if(aaaa.find(":") > -1):
-        ips.append({
-            "type": "AAAA",
-            "ip": aaaa
-        })
-    else:
-        print("Warning: IPv6 not detected.")
+    if not onlyIPv4:
+        aaaa = ""
+        try:
+            aaaa = requests.get("https://api6.ipify.org?format=json").json().get("ip")
+        except Exception:
+            print("Warning: IPv6 not detected.")
+
+        if(aaaa.find(":") > -1):
+            ips.append({
+                "type": "AAAA",
+                "ip": aaaa
+            })
+        else:
+            print("Warning: IPv6 not detected.")
 
     return ips
 
@@ -117,17 +126,14 @@ def updateIPs():
     for ip in getIPs():
         commitRecord(ip)
 
-if(len(sys.argv) > 1):
-    if(sys.argv[1] == "--repeat"):
-        print("Updating A & AAAA records every 10 minutes")
+if(args.repeat > 0):
+    delay = args.repeat
+    print("Updating records every", delay, "seconds")
+    updateIPs()
+    next_time = time.time() + delay
+    while True:
+        time.sleep(max(0, next_time - time.time()))
         updateIPs()
-        delay = 10*60 # 10 minutes
-        next_time = time.time() + delay
-        while True:
-            time.sleep(max(0, next_time - time.time()))
-            updateIPs()
-            next_time += (time.time() - next_time) // delay * delay + delay
-    else:
-        print("Unrecognized parameter '" + sys.argv[1] + "'. Stopping now.")
+        next_time += (time.time() - next_time) // delay * delay + delay
 else:
     updateIPs()
